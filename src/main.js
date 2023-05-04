af582c80c2c70950ab1bb68348d57c4254595689
theringsofsaturn <emiliankasemi@gmail.com>
2023-05-04 16:53:39 +0200

    Added textures, paintings, lights and design improvements

--- src/main.js
+++ src/main.js
@@ -19,106 +19,6 @@ renderer.setSize(window.innerWidth, window.innerHeight); // set size of renderer
 renderer.setClearColor(0xffffff, 1); //background color
 document.body.appendChild(renderer.domElement); // add renderer to html
 
-// Ambient light is a soft light that lights up all the objects in the scene equally
-const ambientLight = new THREE.AmbientLight(0xffffff, 1.0); // color, intensity, distance, decay
-scene.add(ambientLight);
-
-// Directional light is a light source that acts like the sun, that illuminates all objects in the scene equally from a specific direction.
-const sunLight = new THREE.DirectionalLight(0xdddddd, 1.0); // color, intensity, distance, decay
-sunLight.position.y = 15;
-scene.add(sunLight);
-
-const geometry = new THREE.BoxGeometry(1, 1, 1); // BoxGeometry is the shape of the object
-const material = new THREE.MeshBasicMaterial({ color: 'blue' }); // MeshBasicMaterial is the look of the object (color or texture)
-const cube = new THREE.Mesh(geometry, material); // create cube with geometry and material
-scene.add(cube); // add cube to scene
-
-// Controls
-// Event Listenet for when we press the keys
-document.addEventListener('keydown', onKeyDown, false);
-
-// Texture of the floor
-const textureLoader = new THREE.TextureLoader();
-const floorTexture = textureLoader.load('img/Floor.jpg');
-floorTexture.wrapS = THREE.RepeatWrapping; // wrapS is horizonatl direction
-floorTexture.wrapT = THREE.RepeatWrapping; // wrapT the vertical direction
-floorTexture.repeat.set(20, 20); // how many times to repeat the texture
-
-// let floorTexture = new THREE.TextureLoader().load('img/Floor.jpg');
-// textureLoader.load('img/Floor.jpg');cds
-
-// Create the floor plane.
-const planeGeometry = new THREE.PlaneGeometry(45, 45); // BoxGeometry is the shape of the object
-const planeMaterial = new THREE.MeshBasicMaterial({
-  // MeshBasicMaterial is the look of the object (color or texture)
-  map: floorTexture, // the texture
-  side: THREE.DoubleSide,
-});
-
-const floorPlane = new THREE.Mesh(planeGeometry, planeMaterial); // create the floor with geometry and material
-
-floorPlane.rotation.x = Math.PI / 2; // this is 90 degrees
-floorPlane.position.y = -Math.PI; // this is -180 degrees
-
-scene.add(floorPlane); // add the floor to the scene
-
-// Create the walls
-let wallGroup = new THREE.Group(); // create a group to hold the walls
-scene.add(wallGroup); // add the group to the scene, then any child added to the group will display to the scene too
-
-// Front Wall
-const frontWall = new THREE.Mesh( // Mesh class that has geometry and material inside
-  new THREE.BoxGeometry(50, 20, 0.001), // geometry
-  new THREE.MeshLambertMaterial({ color: 'green' }) // Lambert material is for non-shiny surfaces
-);
-
-frontWall.position.z = -20; // push the wall forward in the Z axis
-
-// Left Wall
-const leftWall = new THREE.Mesh( // Mesh class that has geometry and material inside
-  new THREE.BoxGeometry(50, 20, 0.001), // geometry
-  new THREE.MeshLambertMaterial({
-    //  Lambert material is for non-shiny surfaces
-    color: 'red',
-  })
-);
-
-leftWall.rotation.y = Math.PI / 2; // this is 90 degrees
-leftWall.position.x = -20; // -20 is for 20 units left
-
-// Right Wall
-const rightWall = new THREE.Mesh( // Mesh class that has geometry and material inside
-  new THREE.BoxGeometry(50, 20, 0.001), // geometry
-  new THREE.MeshLambertMaterial({
-    // Lambert material is for non-shiny surfaces
-    color: 'yellow',
-  })
-);
-
-rightWall.position.x = 20;
-rightWall.rotation.y = Math.PI / 2; // this is 90 degrees
-
-wallGroup.add(frontWall, leftWall, rightWall); // add the walls to the group
-
-// Loop through each wall and create the bounding box
-for (let i = 0; i < wallGroup.children.length; i++) {
-  wallGroup.children[i].BBox = new THREE.Box3();
-  wallGroup.children[i].BBox.setFromObject(wallGroup.children[i]);
-}
-
-// Create the ceiling
-const ceilingGeometry = new THREE.PlaneGeometry(50, 50);
-const ceilingMaterial = new THREE.MeshLambertMaterial({
-  // Lambert material is for non-shiny surfaces
-  color: 'blue',
-});
-const ceilingPlane = new THREE.Mesh(ceilingGeometry, ceilingMaterial); // create ceiling with geometry and material
-
-ceilingPlane.rotation.x = Math.PI / 2; // this is 90 degrees
-ceilingPlane.position.y = 12;
-
-scene.add(ceilingPlane);
-
 // Create a painting
 function createPainting(imageURL, width, height, position) {
   const textureLoader = new THREE.TextureLoader();
@@ -147,13 +47,172 @@ const painting2 = createPainting(
   new THREE.Vector3(10, 5, -19.99)
 );
 
-scene.add(painting1, painting2);
+// Painting on the left wall
+const painting3 = createPainting(
+  '/artworks/3.jpg',
+  10,
+  5,
+  new THREE.Vector3(-19.99, 5, -10)
+);
+painting3.rotation.y = Math.PI / 2;
+
+// Painting on the right wall
+const painting4 = createPainting(
+  '/artworks/5.jpg',
+  10,
+  5,
+  new THREE.Vector3(19.99, 5, -10)
+);
+painting4.rotation.y = -Math.PI / 2;
+
+scene.add(painting1, painting2, painting3, painting4);
+
+// We can use a combination of ambient light and spotlights to create a more natural and immersive lighting environment.
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+scene.add(ambientLight);
+
+// Spotlights can be used to simulate ceiling-mounted lights or track lights that focus on specific areas or artworks.
+function createSpotlight(x, y, z, intensity, targetPosition) {
+  const spotlight = new THREE.SpotLight(0xffffff, intensity);
+  spotlight.position.set(x, y, z);
+  spotlight.target.position.copy(targetPosition);
+  spotlight.castShadow = true;
+  spotlight.angle = Math.PI / 6; // 30 degrees
+  spotlight.penumbra = 0.9;
+  spotlight.decay = 2;
+  spotlight.distance = 40;
+  spotlight.shadow.mapSize.width = 1024;
+  spotlight.shadow.mapSize.height = 1024;
+  return spotlight;
+}
+
+// Add spotlights to the scene
+const spotlight1 = createSpotlight(-15, 20, -10, 1.5, painting1.position);
+const spotlight2 = createSpotlight(15, 20, -10, 1.5, painting2.position);
+const spotlight3 = createSpotlight(-35, 20, -10, 1.5, painting3.position);
+const spotlight4 = createSpotlight(35, 20, -10, 1.5, painting4.position);
+
+// Add new spotlights to the scene
+scene.add(spotlight3, spotlight4);
+scene.add(spotlight3.target);
+scene.add(spotlight4.target);
+
+scene.add(spotlight1, spotlight2);
+scene.add(spotlight1.target);
+scene.add(spotlight2.target);
+
+// Texture of the floor
+const textureLoader = new THREE.TextureLoader();
+const floorTexture = textureLoader.load('img/floor.png');
+floorTexture.wrapS = THREE.RepeatWrapping; // wrapS is horizonatl direction
+floorTexture.wrapT = THREE.RepeatWrapping; // wrapT the vertical direction
+floorTexture.repeat.set(20, 20); // how many times to repeat the texture
+
+// Create the floor plane.
+const planeGeometry = new THREE.PlaneGeometry(45, 45); // BoxGeometry is the shape of the object
+const planeMaterial = new THREE.MeshPhongMaterial({
+  map: floorTexture, // the texture
+  side: THREE.DoubleSide,
+});
+
+const floorPlane = new THREE.Mesh(planeGeometry, planeMaterial); // create the floor with geometry and material
+
+floorPlane.rotation.x = Math.PI / 2; // this is 90 degrees
+floorPlane.position.y = -Math.PI; // this is -180 degrees
+
+scene.add(floorPlane); // add the floor to the scene
+
+// Create the walls
+let wallGroup = new THREE.Group(); // create a group to hold the walls
+scene.add(wallGroup); // add the group to the scene, then any child added to the group will display to the scene too
+
+// Create wall material with realistic colors and texture
+const wallTexture = textureLoader.load('img/white-texture.jpg');
+wallTexture.wrapS = THREE.RepeatWrapping;
+wallTexture.wrapT = THREE.RepeatWrapping;
+wallTexture.repeat.set(1, 1);
+
+const wallMaterial = new THREE.MeshLambertMaterial({ map: wallTexture });
+
+// Front Wall
+const frontWall = new THREE.Mesh( // Mesh class that has geometry and material inside
+  new THREE.BoxGeometry(85, 20, 0.001), // geometry
+  new THREE.MeshLambertMaterial({ map: wallTexture })
+);
+
+frontWall.position.z = -20; // push the wall forward in the Z axis
+
+// Left Wall
+const leftWall = new THREE.Mesh( // Mesh class that has geometry and material inside
+  new THREE.BoxGeometry(80, 20, 0.001), // geometry
+  new THREE.MeshLambertMaterial({ map: wallTexture })
+);
+
+leftWall.rotation.y = Math.PI / 2; // this is 90 degrees
+leftWall.position.x = -20; // -20 is for 20 units left
+
+// Right Wall
+const rightWall = new THREE.Mesh( // Mesh class that has geometry and material inside
+  new THREE.BoxGeometry(80, 20, 0.001), // geometry
+  new THREE.MeshLambertMaterial({ map: wallTexture })
+);
+
+rightWall.position.x = 20;
+rightWall.rotation.y = Math.PI / 2; // this is 90 degrees
+
+// Back Wall
+const backWall = new THREE.Mesh(
+  new THREE.BoxGeometry(85, 20, 0.001),
+  new THREE.MeshLambertMaterial({ map: wallTexture })
+);
+backWall.position.z = 20;
+
+wallGroup.add(frontWall, backWall, leftWall, rightWall); // add the walls to the group
+
+// Loop through each wall and create the bounding box
+for (let i = 0; i < wallGroup.children.length; i++) {
+  wallGroup.children[i].BBox = new THREE.Box3();
+  wallGroup.children[i].BBox.setFromObject(wallGroup.children[i]);
+}
+
+// Create the ceiling
+const ceilingTexture = textureLoader.load('img/white-texture.jpg');
+const ceilingGeometry = new THREE.PlaneGeometry(45, 40);
+const ceilingMaterial = new THREE.MeshLambertMaterial({ map: ceilingTexture });
+const ceilingPlane = new THREE.Mesh(ceilingGeometry, ceilingMaterial); // create ceiling with geometry and material
+
+ceilingPlane.rotation.x = Math.PI / 2; // this is 90 degrees
+ceilingPlane.position.y = 10;
+
+scene.add(ceilingPlane);
+
+// Optimize the lights and shadows
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+// Enable shadows on objects
+floorPlane.receiveShadow = true;
+ceilingPlane.receiveShadow = true;
+frontWall.castShadow = true;
+frontWall.receiveShadow = true;
+leftWall.castShadow = true;
+leftWall.receiveShadow = true;
+rightWall.castShadow = true;
+rightWall.receiveShadow = true;
+backWall.castShadow = true;
+backWall.receiveShadow = true;
+painting1.castShadow = true;
+painting1.receiveShadow = true;
+painting2.castShadow = true;
+painting2.receiveShadow = true;
 
 // Controls
 const controls = new PointerLockControls(camera, document.body);
 
 // Lock the pointer (controls are activated) and hide the menu when the experience starts
 function startExperience() {
+  // Reset clock
+  clock.start();
   // Lock the pointer
   controls.lock();
   // Hide the menu
@@ -177,34 +236,62 @@ function showMenu() {
 
 controls.addEventListener('unlock', showMenu);
 
-// Add the movement (left/right/forward/backward) to the scene. Press the arrow keys or WASD to move
-function onKeyDown(event) {
-  let keycode = event.which;
+const keysPressed = {
+  ArrowUp: false,
+  ArrowDown: false,
+  ArrowLeft: false,
+  ArrowRight: false,
+  w: false,
+  a: false,
+  s: false,
+  d: false,
+};
 
-  // right arrow key
-  if (keycode === 39 || keycode === 68) {
-    controls.moveRight(0.08);
+// Event Listener for when we press the keys
+document.addEventListener(
+  'keydown',
+  (event) => {
+    if (event.key in keysPressed) {
+      keysPressed[event.key] = true;
+    }
+  },
+  false
+);
+
+// Event Listener for when we release the keys
+document.addEventListener(
+  'keyup',
+  (event) => {
+    if (event.key in keysPressed) {
+      keysPressed[event.key] = false;
+    }
+  },
+  false
+);
+
+// Add the movement (left/right/forward/backward) to the scene. Press the arrow keys or WASD to move
+const clock = new THREE.Clock();
+
+function updateMovement(delta) {
+  const moveSpeed = 5 * delta;
+  if (keysPressed.ArrowRight || keysPressed.d) {
+    controls.moveRight(moveSpeed);
   }
-  // left arrow key
-  else if (keycode === 37 || keycode === 65) {
-    controls.moveRight(-0.08);
+  if (keysPressed.ArrowLeft || keysPressed.a) {
+    controls.moveRight(-moveSpeed);
   }
-  // up arrow key
-  else if (keycode === 38 || keycode === 87) {
-    controls.moveForward(0.08);
+  if (keysPressed.ArrowUp || keysPressed.w) {
+    controls.moveForward(moveSpeed);
   }
-  // down arrow key
-  else if (keycode === 40 || keycode === 83) {
-    controls.moveForward(-0.08);
+  if (keysPressed.ArrowDown || keysPressed.s) {
+    controls.moveForward(-moveSpeed);
   }
 }
 
 let render = function () {
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
-  renderer.render(scene, camera); //renders the scene
-
+  const delta = clock.getDelta();
+  updateMovement(delta);
+  renderer.render(scene, camera);
   requestAnimationFrame(render);
 };
 
